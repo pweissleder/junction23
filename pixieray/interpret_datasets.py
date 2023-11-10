c574cbd7463ecd549b3f7c3124079550c22ccf3d
Phillip Graf <75568847+phillipgraf@users.noreply.github.com>
2023-11-10 22:32:29 +0200

    seperate afe ccolumn

--- pixieray/interpret_datasets.py
+++ pixieray/interpret_datasets.py
@@ -14,11 +14,17 @@ with open(json_datei, 'r') as f:
 # DataFrame erstellen
 df = pd.json_normalize(data, sep='_')
 
+df['afe_R_m'] = df['afe'].apply(lambda x: x[1]['m'][0] if len(x) > 1 and x[1] else None)
+df['afe_L_m'] = df['afe'].apply(lambda x: x[0]['m'][0] if len(x) > 0 and x[0] else None)
+df['afe_R_i'] = df['afe'].apply(lambda x: x[1]['i'] if len(x) > 1 and x[1] else None)
+df['afe_L_i'] = df['afe'].apply(lambda x: x[0]['i'] if len(x) > 0 and x[0] else None)
+df.drop('afe', axis=1, inplace=True)
+
 # Erste Übersicht der Daten anzeigen
 print("Erste Übersicht der Daten:")
 print(df.head())
 
-excel_datei = 'first_summary.xlsx'
+excel_datei = 'first_summary_v2.xlsx'
 
 # DataFrame in Excel speichern
 df.to_excel(excel_datei, index=False)
