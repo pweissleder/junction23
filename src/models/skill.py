a9fc8aa7bb31ceba391e0a5554a629619972d0ea
Jonas Düsterwald <jonas.duesterwald@th-deg.de>
2023-11-11 13:14:18 +0200

    Added skills class.

---
+++ src/models/skill.py
@@ -0,0 +1,48 @@
+class GeneralSkill:
+    def __init__(self, skill_name, initial_level=1):
+        self.skill_name = skill_name
+        self.current_level = initial_level
+        self.current_xp = 0
+        self.levels = {}  # Define as needed
+
+    def add_xp(self, xp):
+        self.current_xp += xp
+        self.check_next_level_reached()
+
+    def check_next_level_reached(self):
+        next_level_xp = self.levels.get(self.current_level + 1)
+        if self.current_xp >= next_level_xp:
+            self.current_level += 1
+            self.current_xp -= next_level_xp
+
+
+class Health(GeneralSkill):
+    """
+    TODO: RENAME TO GENERAL PROGRESS?
+
+    This class represents the overall Health  / general skill level of the user.
+    It is progressed by progressing in other Skills (e.g) Walking, Hiking, Swimming.
+
+    """
+
+    def __init__(self):
+        super().__init__("Health")
+        self.level = {
+            1: 100,
+            2: 500,
+            3: 1000,
+            4: 2000,
+            5: 5000,
+        }
+
+
+class WalkingSkill(GeneralSkill):
+    def __init__(self):
+        super().__init__("Walking")
+        self.level = {
+            1: 100,
+            2: 250,
+            3: 500,
+            4: 900,
+            5: 1500
+        }
