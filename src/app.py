5752a237e5b53ca8f0060b854ef1acbeb711c691
Pascal Weißleder <p.weissleder@weptun.de>
2023-11-11 17:47:07 +0100

    Working endpoint "POST /api/shop/buy "

--- src/app.py
+++ src/app.py
@@ -1,7 +1,14 @@
 import json
 
+from firebase_admin import firestore
 from flask import Flask, jsonify, request
 from src.models.avatar import Avatar
+import firebase_admin
+import os
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+from src.models.cosmetic import Cosmetic
 
 app = Flask(__name__)
 
@@ -39,10 +46,7 @@ def init_user():
     avatar.init_challenges()
     avatar_data = json.loads(avatar.to_json())
 
-    import firebase_admin
-    import os
-    from firebase_admin import credentials
-    from firebase_admin import firestore
+
 
     current_directory = os.getcwd()
     service_account_key_path = os.path.join(current_directory, 'config', 'serviceAccountKey.json')
@@ -76,24 +80,51 @@ def complete_chall():
 
 
     ## Shop
-@app.route('/shop/buy', methods=['GET', 'POST'])
+@app.route('/api/shop/buy', methods=['GET', 'POST'])
 def shop_buy():
     if request.method == "POST":
+
+        current_directory = os.getcwd()
+        service_account_key_path = os.path.join(current_directory, 'config', 'serviceAccountKey.json')
+
+        # Initialize Firebase Admin SDK with the dynamically determined path
+        cred = credentials.Certificate(service_account_key_path)
+        firebase_admin.initialize_app(cred)
+       
+
         print("Flutter has successfully sent POST data on the buy endpoint")
         data = json.loads(request.data)
-        user_id = data.get('userid')
+        user_id = data.get('user_id')
         cosmetic_id = data.get('cosmetic_id')
         # Initialize Firestore database
         db = firestore.client()
 
         # Create or update the user document in Firestore
         user_ref = db.collection('users').document(user_id)
-        user_ref.set(avatar_data)
+
+        try:
+            doc = user_ref.get()
+            if doc.exists:
+                # Document data is stored in the to_dict() method
+                data = doc.to_dict()
+                inventory = dict(data.get('inventory'))
+
+                if cosmetic := inventory.get(cosmetic_id):
+                    cosmetic['bought'] = True
+                    inventory[cosmetic_id] = cosmetic
+                    data['inventory'] = inventory
+                    user_ref.update(data)
+                else:
+                    print(f"Cosmetic {cosmetic_id} does not exist.")
+            else:
+                print(f"Document {user_id} does not exist.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
         # get Avatar
         # get Inventory
         # find item
         # call buy
-
+        firebase_admin.delete_app(firebase_admin.get_app())
         return jsonify({"message": "Ok"})
 
 
